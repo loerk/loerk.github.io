29e9ce6ca43e8625e193b38c5b181a3a666c3091
loerk <fra.se@posteo.net>
2022-05-08 14:03:13 +0200

    added particles

---
+++ src/particles.js
@@ -0,0 +1,77 @@
+
+tsParticles.load("tsparticles", {
+    particles: {
+        number: {
+            value: 100
+        },
+        color: {
+            value: "random",
+            animation: {
+                enable: true,
+                speed: 50,
+                sync: false
+            }
+        },
+        shape: {
+            type: "none",
+        },
+        size: {
+            value: 5,
+            random: true,
+            animation: {
+                enable: true,
+                speed: 16,
+                minimumValue: 0.1,
+                sync: false
+            }
+        },
+        links: {
+            enable: true,
+            distance: 100,
+            color: "#adff2f",
+            opacity: 0.8,
+            width: 1
+        },
+        move: {
+            enable: true,
+        },
+    },
+    interactivity: {
+        detectsOn: "canvas",
+        events: {
+            onHover: {
+                enable: true,
+                mode: "repulse"
+            },
+            onClick: {
+                enable: true,
+                mode: "bubble"
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                links: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 0.8
+            },
+            repulse: {
+                distance: 200
+            },
+            push: {
+                quantity: 1
+            },
+            remove: {
+                quantity: 2
+            }
+        }
+    },
+    detectRetina: true,
+});
